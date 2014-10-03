c560409ed2d90426ba9009883c63a5cc7fdd679b
elc1798 <subliminalmau5@gmail.com>
2014-10-03 15:17:15 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Bash command that adds proxy settings to apt-get, specifically for the nycboe proxy in NYC public schools
+
+How to install:
+
+git clone https://github.com/elc1798/aptproxyget.git
+cd aptproxyget
+sh install.sh
---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo mkdir ~/.aptproxyget
+sudo cp 01proxy.aptproxyget.data  ~/.aptproxyget/01proxy.aptproxyget.data
+sudo cp apt-proxy-get.sh ~/.aptproxyget/apt-proxy-get.sh
+sudo echo alias aptproxyget='sh ~/.aptproxyget/apt-proxy-get.sh' >> ~/.bash_aliases
+source ~/.bashrc
+clear
+echo aptproxyget has been successfully installed.
+echo "Commands:"
+echo "		-c		Checks /etc/apt/apt.conf.d/01proxy existence and outputs the contents of the file"
+echo "		-a		Adds /etc/apt/apt.conf.d/01proxy and the rules for the NYCBOE proxy"
+echo "		-r		Removed /etc/apt/apt.conf.d/01proxy if it exists"
+echo "		-h		Displays this message"
