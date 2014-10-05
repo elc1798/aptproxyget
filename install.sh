ea0aa79689012cf34865bc4716e07dc1f8c011a2
elc1798 <subliminalmau5@gmail.com>
2014-10-05 19:29:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 sudo mkdir ~/.aptproxyget
 sudo cp 01proxy.aptproxyget.data  ~/.aptproxyget/01proxy.aptproxyget.data
 sudo cp apt-proxy-get.sh ~/.aptproxyget/apt-proxy-get.sh
-if [ -e "~/.bash_aliases" ]; then
-  sudo cp ~/.bashaliases alias_data
+cd
+if [ -e ".bash_aliases" ]; then
+  sudo cp .bashaliases alias_data
   sudo echo "alias aptproxyget='sh ~/.aptproxyget/apt-proxy-get.sh'" >> alias_data
-  sudo mv alias_data ~/.bash_aliases
+  sudo mv alias_data .bash_aliases
 else
   touch alias_data
   echo "alias aptproxyget='sh ~/.aptproxyget/apt-proxy-get.sh'" >> alias_data
-  sudo mv alias_data ~/.bash_aliases
+  sudo mv alias_data .bash_aliases
 fi
-source ~/.bashrc
+source .bashrc
 clear
 echo aptproxyget has been successfully installed.
 echo "Commands:"
